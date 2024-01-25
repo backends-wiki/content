91c8d49a141538725a58aff8d14651fc2767a3ff
Alexander <moneyharassment@rambler.ru>
2024-01-25 23:55:35 +0300

    feat: clean docosaurus

---
+++ docusaurus.config.ts
@@ -0,0 +1,68 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'Golang Knowledge Bases',
+  tagline: 'Общедоступная база знаний по Golang',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://golangreview.ru',
+  baseUrl: '/golangreview',
+
+  organizationName: 'golangreview', // Usually your GitHub org/user name.
+  projectName: 'golangreview', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'ru',
+    locales: ['ru'],
+  },
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'Golang Review',
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'База знаний',
+        },
+        {
+          type: 'localeDropdown',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/golangreview/golangreview',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.jettwaveDark,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
