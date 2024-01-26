5dfa775f9fa6f2fcd6a625147c9275a6817d99e1
Alexander <moneyharassment@rambler.ru>
2024-01-26 14:37:12 +0300

    feat: edit this page button

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -30,6 +30,7 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
+          editUrl: 'https://github.com/golangreview/golangreview/tree/main',
         },
         theme: {
           customCss: './src/css/custom.css',
