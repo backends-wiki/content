7d2299501600696c13ccf2c2fd190741de37e014
Alexander <moneyharassment@rambler.ru>
2024-03-28 21:19:22 +0300

    add data-management in sysdesign

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -50,7 +50,6 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
       title: 'Golang Review',
